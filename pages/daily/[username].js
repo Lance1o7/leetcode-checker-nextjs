2bad7d1e072dc74c9e645a4f108a021eeb6e2161
Lance <10292888+Lance1o7@users.noreply.github.com>
2023-01-09 20:41:22 -0500

    Align and add logo

--- pages/daily/[username].js
+++ pages/daily/[username].js
@@ -7,8 +7,16 @@ const Username = () => {
     const router = useRouter()
     const { username } = router.query;
     const { data, error } = useSWR('https://leetcode-checker.onrender.com/api/day/' + username, fetcher)
-    if (error) return <code>Failed to load. Please check your username</code>
-    if (!data) return <code>Loading...</code>
+    if (error) return (
+        <main className='content'>
+            <code>Failed to load. Please check your username</code>
+        </main>
+    )
+    if (!data) return (
+        <main className='content'>
+            <code>Loading...</code>
+        </main>
+    )
     const options = {
         weekday: "long",
         year: "numeric",
@@ -16,20 +24,28 @@ const Username = () => {
         day: "numeric",
     };
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <div className="card-body">
-                <h1 className="card-title"> {new Date().toLocaleString("en-US", options)}
-                </h1>
-                <p> <code>birdtosky</code> finished 6 problems</p>
-                <div className='form-control'>
-                    {data.map((entry, index) => (
-                        <label className="cursor-pointer label" key={index}>
-                            <span className="label-text"><a href='https://leetcode.com/'>{entry.title}</a></span>
-                            <input type="checkbox" checked="checked" readOnly={true} className="checkbox checkbox-success checkbox-xs" />
-                        </label>
-                    ))}</div>
+        <main className='content'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className='w-96'><img src="/logo.png" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h1 className="card-title"> {new Date().toLocaleString("en-US", options)}</h1>
+                    <p> <code>birdtosky</code> finished 6 problems</p>
+                    <div className='form-control'>
+                        {
+                            data.map((entry, index) => (
+                                <label className="cursor-auto label" key={index}>
+                                    <span className="label-text font-bold"><a href={`https://leetcode.com/problems/${entry.titleSlug}`}>{entry.title} &nbsp;<code className='text-xs font-thin text-gray-300 uppercase'>{entry.difficulty}</code></a></span>
+                                    <input type="checkbox" checked="checked" readOnly={true} className="object-none object-right checkbox checkbox-success checkbox-xs" />
+                                </label>
+
+                            ))
+                        }
+                    </div>
+
+                </div>
             </div>
-        </div>
+        </main >
+
     )
 }
 
