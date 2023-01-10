33bacc17bb834461f27f3d947088c707812d66b1
Lance <10292888+Lance1o7@users.noreply.github.com>
2023-01-10 12:53:48 -0500

    Add @vercel/analytic

--- pages/daily/[username].js
+++ pages/daily/[username].js
@@ -5,6 +5,7 @@ import exportAsImage from "../../components/exportAsImage.js";
 import { faEyeSlash, faImage, faEye } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Analytics } from "@vercel/analytics/react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -40,78 +41,90 @@ const Username = (props) => {
   };
 
   return (
-    <main className="w-100">
-      <div className="container">
-        <div ref={exportRef} className="card w-96 bg-base-100 drop-shadow-2xl">
-          <figure className="w-96">
-            <img
-              src="/light.png"
-              className="block dark:hidden"
-              alt="LeetCode Logo"
-            />
-            <img
-              src="/dark.png"
-              className="hidden dark:block"
-              alt="LeetCode Logo"
-            />
-          </figure>
-          <div className="card-body">
-            <h1 className="card-title">
-              {" "}
-              {new Date().toLocaleString("en-US", options)}
-            </h1>
-            <p className={isActive ? "blurred" : null}>
-              {" "}
-              <span>
-                <code>{username}</code>
-              </span>{" "}
-              finished {data.length} problems
-            </p>
-            <div className="form-control">
-              {data.map((entry, index) => (
-                <label className="cursor-auto label" key={index}>
-                  <span className="label-text font-bold">
-                    <a
-                      href={`https://leetcode.com/problems/${entry.titleSlug}`}
-                    >
-                      {entry.title} &nbsp;
-                      <code className="text-xs font-thin text-gray-300 uppercase">
-                        {entry.difficulty}
-                      </code>
-                    </a>
-                  </span>
-                  <input
-                    type="checkbox"
-                    checked="checked"
-                    readOnly={true}
-                    className="object-none object-right checkbox checkbox-success checkbox-xs"
-                  />
-                </label>
-              ))}
+    <>
+      <main className="w-100">
+        <div className="container">
+          <div
+            ref={exportRef}
+            className="card w-96 bg-base-100 drop-shadow-2xl"
+          >
+            <figure className="w-96">
+              <img
+                src="/light.png"
+                className="block dark:hidden"
+                alt="LeetCode Logo"
+              />
+              <img
+                src="/dark.png"
+                className="hidden dark:block"
+                alt="LeetCode Logo"
+              />
+            </figure>
+            <div className="card-body">
+              <h1 className="card-title">
+                {" "}
+                {new Date().toLocaleString("en-US", options)}
+              </h1>
+              <p className={isActive ? "blurred" : null}>
+                {" "}
+                <span>
+                  <code>{username}</code>
+                </span>{" "}
+                finished {data.length} problems
+              </p>
+              <div className="form-control">
+                {data.map((entry, index) => (
+                  <label className="cursor-auto label" key={index}>
+                    <span className="label-text font-bold">
+                      <a
+                        href={`https://leetcode.com/problems/${entry.titleSlug}`}
+                      >
+                        {entry.title} &nbsp;
+                        <code className="text-xs font-thin text-gray-300 uppercase">
+                          {entry.difficulty}
+                        </code>
+                      </a>
+                    </span>
+                    <input
+                      type="checkbox"
+                      checked="checked"
+                      readOnly={true}
+                      className="object-none object-right checkbox checkbox-success checkbox-xs"
+                    />
+                  </label>
+                ))}
+              </div>
             </div>
           </div>
+          <div className="container py-10 px-10 mx-0 inline-flex justify-evenly	 items-center">
+            <button
+              title="Export as image"
+              onClick={() => {
+                exportAsImage(
+                  exportRef.current,
+                  new Date().toLocaleString("zh-CN") + "_" + username
+                );
+              }}
+            >
+              <FontAwesomeIcon icon={faImage} size="lg" />
+            </button>
+            <button
+              title="Toggle hiding username"
+              onClick={() => toggleClass()}
+            >
+              <FontAwesomeIcon
+                icon={!isActive ? faEyeSlash : faEye}
+                size="lg"
+              />
+            </button>
+            <a href="https://github.com/Lance1o7/leetcode-checker-nextjs">
+              <FontAwesomeIcon icon={faGithub} size="lg" />
+            </a>
+          </div>
         </div>
-        <div className="container py-10 px-10 mx-0 inline-flex justify-evenly	 items-center">
-          <button
-            title="Export as image"
-            onClick={() => {
-              exportAsImage(
-                exportRef.current,
-                new Date().toLocaleString("zh-CN") + "_" + username
-              );
-            }}
-          >
-            <FontAwesomeIcon icon={faImage} size="lg" />
-          </button>
-          <button title="Toggle hiding username" onClick={() => toggleClass()}>
-            <FontAwesomeIcon icon={!isActive ? faEyeSlash : faEye} size="lg" />
-          </button>
-          <a href="https://github.com/Lance1o7/leetcode-checker-nextjs">
-            <FontAwesomeIcon icon={faGithub} size="lg" />
-          </a>
-        </div>
-      </div>
-    </main>
+      </main>
+      <Analytics />
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   const router = useRouter();
@@ -12,52 +13,55 @@ export default function Home() {
     router.push("daily/" + route);
   };
   return (
-    <main className="w-100">
-      <div className="container">
-        <div className="card w-96 bg-base-100 drop-shadow-2xl">
-          <figure className="w-96">
-            <img
-              src="/light.png"
-              className="block dark:hidden"
-              alt="LeetCode Logo"
-            />
-            <img
-              src="/dark.png"
-              className="hidden dark:block"
-              alt="LeetCode Logo"
-            />
-          </figure>
-          <div className="card-body">
-            <h1 className="card-title flex flex-row justify-center items-center">
-              Please Input your LeetCode ID
-            </h1>
-            <p className="text-xs	flex flex-row justify-center items-center">
-              Data will be updated every 1 hour.
-            </p>
-            <form
-              className="form-control inline-flex justify-center"
-              onSubmit={handleSubmit}
-            >
-              <input
-                type="text"
-                placeholder="JohnDoe"
-                onChange={(e) => {
-                  setRoute(e.target.value);
-                }}
-                className="input input-bordered max-w-xs mt-3"
+    <>
+      <main className="w-100">
+        <div className="container">
+          <div className="card w-96 bg-base-100 drop-shadow-2xl">
+            <figure className="w-96">
+              <img
+                src="/light.png"
+                className="block dark:hidden"
+                alt="LeetCode Logo"
               />
-              <button type="submit" className="btn mt-5">
-                Submit
-              </button>
-            </form>
+              <img
+                src="/dark.png"
+                className="hidden dark:block"
+                alt="LeetCode Logo"
+              />
+            </figure>
+            <div className="card-body">
+              <h1 className="card-title flex flex-row justify-center items-center">
+                Please Input your LeetCode ID
+              </h1>
+              <p className="text-xs	flex flex-row justify-center items-center">
+                Data will be updated every 1 hour.
+              </p>
+              <form
+                className="form-control inline-flex justify-center"
+                onSubmit={handleSubmit}
+              >
+                <input
+                  type="text"
+                  placeholder="JohnDoe"
+                  onChange={(e) => {
+                    setRoute(e.target.value);
+                  }}
+                  className="input input-bordered max-w-xs mt-3"
+                />
+                <button type="submit" className="btn mt-5">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="container py-10 px-10 mx-0 flex justify-evenly items-center">
+            <a href="https://github.com/Lance1o7/leetcode-checker-nextjs">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
           </div>
         </div>
-        <div className="container py-10 px-10 mx-0 flex justify-evenly items-center">
-          <a href="https://github.com/Lance1o7/leetcode-checker-nextjs">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-        </div>
-      </div>
-    </main>
+      </main>
+      <Analytics />
+    </>
   );
 }
