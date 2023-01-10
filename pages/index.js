6329b5c03b0c43c466809a5e2303f646ef4067fb
Lance <10292888+Lance1o7@users.noreply.github.com>
2023-01-10 11:58:51 -0500

    Add navigation page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,60 @@
 import React from "react";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 export default function Home() {
-  React.useEffect(() => {
-    window.location.replace(
-      "https://github.com/Lance1o7/leetcode-checker-nextjs"
-    );
-  }, []);
+  const router = useRouter();
+  const [route, setRoute] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push("daily/" + route);
+  };
+  return (
+    <main className="w-100">
+      <div className="container">
+        <div className="card w-96 bg-base-100 drop-shadow-2xl">
+          <figure className="w-96">
+            <img
+              src="/light.png"
+              className="block dark:hidden"
+              alt="LeetCode Logo"
+            />
+            <img
+              src="/dark.png"
+              className="hidden dark:block"
+              alt="LeetCode Logo"
+            />
+          </figure>
+          <div className="card-body">
+            <h1 className="card-title flex flex-row justify-center items-center">
+              Please Input your LeetCode ID
+            </h1>
+            <form
+              className="form-control w-full max-w-xs"
+              onSubmit={handleSubmit}
+            >
+              <input
+                type="text"
+                placeholder="JohnDoe"
+                onChange={(e) => {
+                  setRoute(e.target.value);
+                }}
+                className="input input-bordered w-full max-w-xs mt-3"
+              />
+              <button type="submit" className="btn mt-5">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="container py-10 px-10 mx-0 inline-flex justify-evenly	 items-center">
+          <a href="https://github.com/Lance1o7/leetcode-checker-nextjs">
+            <FontAwesomeIcon icon={faCode} />
+          </a>
+        </div>
+      </div>
+    </main>
+  );
 }
