47a00fcc9a728e49c0e95d6ec5c30605793b1937
Lance <10292888+Lance1o7@users.noreply.github.com>
2023-01-09 22:00:45 -0500

    Add export as image feature

--- pages/daily/[username].js
+++ pages/daily/[username].js
@@ -1,8 +1,12 @@
 import { useRouter } from "next/router";
+import React, { useRef, useEffect, useState } from "react";
 import useSWR from "swr";
+import exportAsImage from "./exportAsImage";
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const Username = () => {
+  const exportRef = useRef();
   const router = useRouter();
   const { username } = router.query;
   const { data, error } = useSWR(
@@ -30,39 +34,56 @@ const Username = () => {
 
   return (
     <main className="content">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure className="w-96">
-          <img src="/logo.png" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h1 className="card-title">
-            {" "}
-            {new Date().toLocaleString("en-US", options)}
-          </h1>
-          <p>
-            {" "}
-            <code>{username}</code> finished {data.length} problems
-          </p>
-          <div className="form-control">
-            {data.map((entry, index) => (
-              <label className="cursor-auto label" key={index}>
-                <span className="label-text font-bold">
-                  <a href={`https://leetcode.com/problems/${entry.titleSlug}`}>
-                    {entry.title} &nbsp;
-                    <code className="text-xs font-thin text-gray-300 uppercase">
-                      {entry.difficulty}
-                    </code>
-                  </a>
-                </span>
-                <input
-                  type="checkbox"
-                  checked="checked"
-                  readOnly={true}
-                  className="object-none object-right checkbox checkbox-success checkbox-xs"
-                />
-              </label>
-            ))}
+      <div className="container">
+        <div ref={exportRef} className="card w-96 bg-base-100 shadow-xl">
+          <figure className="w-96">
+            <img src="/logo.png" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h1 className="card-title">
+              {" "}
+              {new Date().toLocaleString("en-US", options)}
+            </h1>
+            <p>
+              {" "}
+              <code>{username}</code> finished {data.length} problems
+            </p>
+            <div className="form-control">
+              {data.map((entry, index) => (
+                <label className="cursor-auto label" key={index}>
+                  <span className="label-text font-bold">
+                    <a
+                      href={`https://leetcode.com/problems/${entry.titleSlug}`}
+                    >
+                      {entry.title} &nbsp;
+                      <code className="text-xs font-thin text-gray-300 uppercase">
+                        {entry.difficulty}
+                      </code>
+                    </a>
+                  </span>
+                  <input
+                    type="checkbox"
+                    checked="checked"
+                    readOnly={true}
+                    className="object-none object-right checkbox checkbox-success checkbox-xs"
+                  />
+                </label>
+              ))}
+            </div>
           </div>
+        </div>
+        <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+          <button
+            className="btn"
+            onClick={() =>
+              exportAsImage(
+                exportRef.current,
+                new Date().toLocaleString("zh-CN") + "_" + username
+              )
+            }
+          >
+            Export
+          </button>
         </div>
       </div>
     </main>
